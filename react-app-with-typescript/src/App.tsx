2747604680694f27885049cad660c566c6f75e06
Federico Iglesia <federico.iglesia@outlook.com>
2022-10-25 17:09:52 -0300

    interfaces

--- react-app-with-typescript/src/App.tsx
+++ react-app-with-typescript/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+interface Sub {
+  nick: string;
+  avatar: string;
+  subMonths: number;
+  description?: string;
+}
 
 function App() {
+  // const [number, setNumber] = useState<number | string>(5);
+  const [subs, setSubs] = useState<Array<Sub>>([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Subs</h1>
+      <ul>
+        {subs.map((sub) => {
+          return (
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+              <h4>
+                {sub.nick}(<small>{sub.subMonths}</small>)
+              </h4>
+              <p>{sub.description?.substring(0, 100)}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
